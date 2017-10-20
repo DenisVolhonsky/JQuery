37841527d9752e97d6c85ea13091dbdf68cbe620
DenisVolhonsky <dvolhonsky@gmail.com>
2017-10-20 22:16:24 +0300

    form complete!

--- jquery21/js/script.js
+++ jquery21/js/script.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {   //загружается документ когда он готов выполняется функция
 	$('#submit').on('click', function() {
-	 //    if($('input').val().length===0 &&) $('option').attr("value") == 0) {
-        //        alert('Заполните все поля!');
-        //     }
-        //     else alert('Ваша форма отправлена')
         var a={};
-        if($('#ch1').is(':checked')){
-            a.name=$('input[name=FirstName]').val();
-            a.lastname=$('input[name=LastName]').val();
-            a.age=$('#age').val();
-            a.sex=$('input[name=sex]:checked').val();
-            a.date=$('#date').val();
-            a.vithMe=$('#sup option:selected').val();
-            a.region=$('#region option:selected').val();
-            a.tel=$('#tel').val();
-            a.mail=$('#mail').val();
+        if($('#ch1').is(':checked')) {
+            a.name = $('input[name=FirstName]').val();
+            a.lastname = $('input[name=LastName]').val();
+            a.age = $('#age').val();
+            a.sex = $('input[name=sex]:checked').val();
+            a.date = $('#date').val();
+            a.withMe = $('#sup option:selected').val();
+            a.region = $('#region option:selected').val();
+            a.tel = $('#tel').val();
+            a.mail = $('#mail').val();
             console.log(a);
+
+            for (var key in a) {
+                if (a[key] == '') alert('Заполните поле:' + key);
+            }
+            if (a.region == 0) alert('Выберите регион');
+            if (a.sex === undefined) alert('Введите пол');
+            if (a.withMe== -1) alert('Введите кол-во сопровождающих');
         }
         else{
-            alert('Приймите соглашение');
+            alert('Приймите лицензионное соглашение!');
         }
-        for(var key in a) {
-            if(a[key]==='')
-                console.log('заполните все!');
-        }
-         });
 
 
+    });
 });
